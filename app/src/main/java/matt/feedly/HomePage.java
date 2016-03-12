c43935e204bc35e7f41fdaf8ed07b9a8eeafd313
sage365 <bharatgoel36@gmail.com>
2016-03-12 15:31:10 +0530

    Homepage done.

--- app/src/main/java/matt/feedly/HomePage.java
+++ app/src/main/java/matt/feedly/HomePage.java
@@ -22,8 +22,11 @@ public class HomePage extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+
     }
+
 
 }
