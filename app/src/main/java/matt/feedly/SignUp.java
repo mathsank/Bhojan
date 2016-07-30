304797a127a8b51d7324d1e06f8c8865c7f91fe7
mathsank <mathsank@gmail.com>
2016-07-30 16:15:22 +0530

    Map basic2

--- app/src/main/java/matt/feedly/SignUp.java
+++ app/src/main/java/matt/feedly/SignUp.java
@@ -2,20 +2,33 @@ package matt.feedly;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class SignUp extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
+    private static final String REGISTER_URL = "http://bhojansvce.16mb.com/insert2.php";
 
     @InjectView(R.id.input_name) EditText _nameText;
     @InjectView(R.id.input_email) EditText _emailText;
@@ -46,7 +59,9 @@ public class SignUp extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
 
     public void signup() {
         Log.d(TAG, "Signup");
@@ -55,11 +70,14 @@ public class SignUp extends AppCompatActivity {
             onSignupFailed();
             return;
         }
+        else {
+            onSignupSuccess();
+        }
+
 
         _signupButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(SignUp.this,
-                R.style.AppTheme);
+        final ProgressDialog progressDialog = new ProgressDialog(SignUp.this, R.style.AppTheme);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
@@ -67,6 +85,7 @@ public class SignUp extends AppCompatActivity {
         String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
+        String phno = _phoneNumber.getText().toString();
 
         // TODO: Full SignUp logic checking
 
@@ -86,7 +105,8 @@ public class SignUp extends AppCompatActivity {
     public void onSignupSuccess() {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
-        finish();
+        registerUser();
+
     }
 
     public void onSignupFailed() {
@@ -118,7 +138,6 @@ public class SignUp extends AppCompatActivity {
             _emailText.setError(null);
         }
 
-
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
             _passwordText.setError("Between 4 and 10 alphanumeric characters");
             valid = false;
@@ -144,9 +163,48 @@ public class SignUp extends AppCompatActivity {
         } else {
             _phoneNumber.setError(null);
         }
-
-
-
         return valid;
+    }
+    private void register(String name,String email,String password, String phno) {
+        class RegisterUser extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            RegisterUserClass ruc = new RegisterUserClass();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(SignUp.this, "Please Wait",null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String,String>();
+                data.put("name",params[0]);
+                data.put("email",params[1]);
+                data.put("password",params[2]);
+                data.put("phno",params[3]);
+
+                String result = ruc.sendPostRequest(REGISTER_URL,data);
+                return  result;
+            }
+        }
+
+        RegisterUser ru = new RegisterUser();
+        ru.execute(name,email,password,phno);
+    }
+    private void registerUser() {
+        String name = _nameText.getText().toString().trim().toLowerCase();
+        String password = _passwordText.getText().toString().trim().toLowerCase();
+        String email = _emailText.getText().toString().trim().toLowerCase();
+        String phno = _phoneNumber.getText().toString().trim().toLowerCase();
+        register(name,email,password,phno);
     }
 }
